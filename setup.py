ba71865adf215badc313d10ca068f96c2f5fba26
sylee02082766-droid <sylee02082766@gmail.com>
2026-05-05 22:35:30 +0900

    Add depth_center_node entry point to setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'mec_wheel_node = mec_wheel.mec_wheel_node:main'
+            'mec_wheel_node = mec_wheel.mec_wheel_node:main',
+            'depth_center_node = mec_wheel.depth_center_node:main'
         ],
     },
 )
